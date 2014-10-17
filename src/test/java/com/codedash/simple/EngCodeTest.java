1fb4bf7d6a046ad6a60d25f2596b3d10b6d871b8
vsriram77 <v_sram@yahoo.com>
2014-10-16 17:48:42 -0700

    importing simple sources

---
+++ src/test/java/com/codedash/simple/EngCodeTest.java
@@ -0,0 +1,27 @@
+package com.codedash.simple;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vsriram
+ * Date: 10/5/14
+ * Time: 5:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EngCodeTest {
+    @Test
+    public void testLongValue() throws Exception {
+        Assert.assertEquals(EngCode.longValue("A"), 1);
+        Assert.assertEquals(EngCode.longValue("z"), 26);
+        Assert.assertEquals(EngCode.longValue("AA"), 27);
+    }
+
+    @Test
+    public void testIsEngCode() throws Exception {
+        Assert.assertEquals(EngCode.isEngCode("AA"), true);
+        Assert.assertEquals(EngCode.isEngCode("A1"), false);
+
+    }
+}
