1b4759c06054692d90aa17d486222ef7b0885c34
vsriram77 <v_sram@yahoo.com>
2014-10-19 20:18:06 -0700

    check whether a binary tree is balanced with O(h) additional storage only

---
+++ src/test/java/codedash/codedash/trees/BalancedTreeCheckerTest.java
@@ -0,0 +1,62 @@
+package codedash.codedash.trees;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit tests for BalancedTreeCheckerTest
+ */
+public class BalancedTreeCheckerTest {
+
+    @Test
+    public void testIsBinaryTreeBalanced() throws Exception {
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Node c = new Node("c");
+        Node d = new Node("d");
+        Node e = new Node("e");
+        Node f = new Node("f");
+        Node g = new Node("g");
+        Node h = new Node("h");
+        a.setLeft(b);
+        a.setRight(c);
+        b.setLeft(d);
+        b.setRight(e);
+        c.setLeft(g);
+        e.setLeft(f);
+
+        Assert.assertEquals(BalancedTreeChecker.isBinaryTreeBalanced(a), true);
+
+        f.setLeft(h);
+        Assert.assertEquals(BalancedTreeChecker.isBinaryTreeBalanced(a), false);
+
+        f.setLeft(null);
+        a.setRight(null);
+        Assert.assertEquals(BalancedTreeChecker.isBinaryTreeBalanced(a), false);
+
+        a.setLeft(null);
+        a.setRight(null);
+        Assert.assertEquals(BalancedTreeChecker.isBinaryTreeBalanced(a), true);
+
+    }
+
+    @Test
+    public void testIsBinaryTreeBalanced2() throws Exception {
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Node c = new Node("c");
+        Node d = new Node("d");
+        Node e = new Node("e");
+        Node f = new Node("f");
+        Node g = new Node("g");
+        a.setLeft(b);
+        b.setLeft(c);
+        a.setRight(d);
+        d.setRight(e);
+        e.setRight(f);
+        d.setLeft(g);
+
+        Assert.assertEquals(BalancedTreeChecker.isBinaryTreeBalanced(a), true);
+    }
+
+}
